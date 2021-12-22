a2308670b24d54b010dbc1d4c9d4e0c08f4ad123
Payton-Lee <persurejonas1202@163.com>
2021-12-22 10:21:59 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Addition></Addition>
+    <p>------------------</p>
+    <Subtraction></Subtraction>
+  </div>
+</template>
+
+<script>
+import Addition from "./components/Addition.vue"
+import Subtraction from "./components/Subtraction.vue"
+export default {
+  components: {
+    Addition,
+    Subtraction
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+</style>
