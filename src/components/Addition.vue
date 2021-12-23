a68c4f28f2c8eef37c08ee33f0d75fa8e9f4ed20
Payton-Lee <persurejonas1202@163.com>
2021-12-23 08:48:45 +0800

    jishuqi开发

--- src/components/Addition.vue
+++ src/components/Addition.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h3>当前最新的count值为： </h3>
-    <button>+1</button>
+    <!-- <h3>当前最新的count值为： {{ $store.state.count}}</h3> -->
+    <h3>{{ $store.getters.showNum}}</h3>
+    <button @click="btnhandler1">+1</button>
+    <button @click="btnhandlern">+n</button>
+    <button @click="btnhandlerAsync">+1 Async</button>
+    <button @click="btnhandler4">+n Async</button>
   </div>
 </template>
 
@@ -11,6 +15,24 @@ export default {
     return {
       
     }
+  },
+  methods: {
+    btnhandler1() {
+      this.$store.commit('add')
+    },
+    btnhandlern() {
+      // commit 就是调用某个mutation函数
+      this.$store.commit('addN', 3)
+    },
+    // 异步的让count加一
+    // dispatch代表要触发某个actions
+    btnhandlerAsync() {
+      this.$store.dispatch('addAsync')
+    },
+    btnhandler4() {
+      this.$store.dispatch('addNAsync', 5)
+    }
+
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,57 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0
   },
+  // 只有mutations中定义的函数，才能有权利修改state中的数据
   mutations: {
+    add(state) {
+      // 不要在mutations函数中执行异步操作
+      // setTimeout(function(){
+      //   // 变更状态
+      //   state.count++
+      // })
+      state.count++
+    },
+    addN(state, step){
+      state.count += step
+    },
+    sub(state) {
+      state.count--
+    },
+    subN(state, step) {
+      state.count -= step
+    }
   },
   actions: {
+    addAsync(context) {
+      setTimeout(()=>{
+        // 在actions中，不能直接修改 state中的数据
+        // 必须通过context.commit() 触发某个 mutation 才行
+        context.commit('add')
+      }, 1000)
+    },
+    addNAsync(context, step){
+      setTimeout(()=>{
+        context.commit('addN', step)
+      }, 1000)
+    },
+    subAsync(context) {
+      setTimeout(()=>{
+        context.commit('sub')
+      }, 1000)
+    },
+    subNAsync(context, step){
+      setTimeout(()=> {
+        context.commit('subN', step)
+      }, 1000)
+    }
+
+  },
+  getters: {
+    showNum(state) {
+      return '当前最新的数量是['+  state.count + ']'
+    }
   },
   modules: {
   }
